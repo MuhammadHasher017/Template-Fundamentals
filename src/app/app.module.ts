b82cfa0dfbbccaf815e0dcf52ce75de8ad82fa20
hashermm <m.hasher@micromerger.com>
2021-09-28 10:57:40 +0500

    creating component for rendring flows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RenderingFlowsComponent } from './rendering-flows/rendering-flows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RenderingFlowsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rendering-flows/rendering-flows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rendering-flows',
+  templateUrl: './rendering-flows.component.html',
+  styleUrls: ['./rendering-flows.component.css']
+})
+export class RenderingFlowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
